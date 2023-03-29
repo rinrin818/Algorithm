4ef54a1d239e27eb2829032c9c35df1a6922ec22
rinrin818 <rinrinyy818@gmail.com>
2023-03-29 18:24:40 +0900

    Revert "Delete ct00_3.java"
    This reverts commit bdf4c951380264eb5348b9d4cf167ff61aef9746.

---
+++ src/ct00/ct00_3.java
@@ -0,0 +1,10 @@
+package ct00;
+
+public class ct00_3 {
+//몫 구하기
+	    public int solution(int num1, int num2) {
+	        int answer = 0;
+	        answer = num1/num2;
+	        return answer;
+	    }
+	}
